9ecc18bb498c444c856568fd704cc45600d68704
Rexolt <157226862+Rexolt@users.noreply.github.com>
2025-07-06 17:48:11 +0200

    Fix blank window by disabling context isolation

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const { app, BrowserWindow, globalShortcut, ipcMain, shell } = require('electron');
 const path = require('path');
+const { exec } = require('child_process');
 
 let win;
 function createWindow() {
@@ -14,7 +15,8 @@ function createWindow() {
     skipTaskbar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true
+      contextIsolation: false,
+      nodeIntegration: true
     }
   });
 
@@ -35,8 +37,22 @@ app.whenReady().then(() => {
   });
 
   if (!registered) console.error('Super+Space registration failed');
+
+  ipcMain.on('launch-item', (_, itemPath) => {
+    if (itemPath.endsWith('.desktop')) {
+      const cmd = `gtk-launch ${path.basename(itemPath, '.desktop')}`;
+      exec(cmd);
+    } else {
+      shell.openPath(itemPath);
+    }
+  });
+
+  ipcMain.on('hide-window', () => {
+    if (win) win.hide();
+  });
 });
 
 app.on('will-quit', () => {
   globalShortcut.unregisterAll();
 });
+
